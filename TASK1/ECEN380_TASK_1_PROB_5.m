083e93a32fa8e9e7905285168ead79dfabc73258
Horne467 (Mint Laptop) <horne467@byu.edu>
2025-10-27 17:31:11 -0600

    Add first draft of z-plane H1 and H2 rep.
    Signed-off-by: Horne467 (Mint Laptop) <horne467@byu.edu>

--- TASK1/ECEN380_TASK_1_PROB_5.m
+++ TASK1/ECEN380_TASK_1_PROB_5.m
@@ -3,7 +3,17 @@
 H1_num = [0, 0, 3.5531e8];
 H1_den = [1, 1.4427e4, 3.5531e8];
 
+figure;
+zplane(H1_num,H1_den);
+title("H1(s)");
+grid on;
+
 %% H2(s)
 
 H2_num = [0, 0, 3.5531e8];
 H2_den = [1, 3.4830e4, 3.5531e8];
+
+figure;
+zplane(H2_num,H2_den);
+title("H2(s)");
+grid on;
