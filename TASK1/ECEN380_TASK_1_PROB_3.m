f4736b8e01a9edfabc0689ee93b96266f9bf104e
Horne467 (Mint Laptop) <horne467@byu.edu>
2025-10-27 16:57:39 -0600

    Commented code.
    Signed-off-by: Horne467 (Mint Laptop) <horne467@byu.edu>

--- TASK1/ECEN380_TASK_1_PROB_3.m
+++ TASK1/ECEN380_TASK_1_PROB_3.m
@@ -1,11 +1,18 @@
+% Target angular freq.
 wc = 2*pi*(3e3);
+% Order of filter.
 p = 6;
+% Instantiate Butterworth filter.
 [B, A] = butter(p, wc, 's');
 
+% Throw poles in an array
 poles = roots(A);
 
+% Find angle of poles in radians.
 angle(poles)
 
+% How many (1/4)*pi's are these angles
 angle(poles)/((1/4)*pi)
 
+% Confirm absolute value of these poles.
 abs(poles)
